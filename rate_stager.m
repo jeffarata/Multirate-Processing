122279b3f97d2be03a4c9ac35893728eef357085
jka <jeffarata@comcast.net>
2018-06-01 15:55:26 -0500

    adding rate stager function to find out which factors to interpolate or decimate by based on how many stages of which is desired

---
+++ rate_stager.m
@@ -0,0 +1,46 @@
+% Jeff Arata
+% 4/3/18
+
+function [ sf, Nout ] = rate_stager( R, f )
+% This function takes in a number, breaks it into its prime factors, and
+% condenses those to how ever many factors the user prefers. This will
+% default to 3. More than the number of prime factors will print an error
+% and instead give the most factors possible. These factors will be
+% increasing.
+%
+% Inputs:
+%
+% R -       the rate of conversion to be broken into factors
+% f -       the number of factors to break down R into
+%
+% Outputs:
+% 
+% sf -      the stage factors of R
+% Nout -    the length of sf/the number of actual output stages
+
+if nargin < 2
+    f = 3;
+end
+
+pf = prime_factor(R);   % Get prime factors of R
+
+if length(pf) > f       % When number of factors requested is less than in
+    sf = pf(1:f);       % the number of prime factors
+    
+    % Multiply leftovers in pf to sf vector
+    for ii = (f+1):length(pf)
+        sf(mod(ii-1,f)+1) = sf(mod(ii-1,f)+1) * pf(ii);
+    end             
+    sf = sort(sf);      % Sort to be increasing order
+    
+elseif length(pf) == f
+    sf = pf;    
+else % when length(pf) < f
+    sf = pf;
+    warning(['Requested %1.f factors but, the number %1.f has %1.f factors ' ...
+        'at most. Using %1.f factors.\n'], f, R, length(pf), length(pf)); 
+end
+
+Nout = length(sf);
+
+end
